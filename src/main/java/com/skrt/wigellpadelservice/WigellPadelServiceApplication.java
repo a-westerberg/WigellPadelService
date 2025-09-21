e43627b21d147de06b643adb13d8fef87faeea5a
aaalx <alexander.westerberg.svedberg@gmail.com>
2025-09-21 18:13:07 +0200

    first commit

---
+++ src/main/java/com/skrt/wigellpadelservice/WigellPadelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skrt.wigellpadelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellPadelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellPadelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skrt/wigellpadelservice/WigellPadelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrt.wigellpadelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellPadelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
