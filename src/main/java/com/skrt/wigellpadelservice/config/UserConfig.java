8a037523be1e72b4770f70f82cd37921d4331408
aaalx <alexander.westerberg.svedberg@gmail.com>
2025-09-21 21:21:47 +0200

    SecurityConfig och UserConfig

---
+++ src/main/java/com/skrt/wigellpadelservice/config/UserConfig.java
@@ -0,0 +1,29 @@
+package com.skrt.wigellpadelservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("simon").password(passwordEncoder.encode("simon")).roles("ADMIN").build(),
+                User.withUsername("alex").password(passwordEncoder.encode("alex")).roles("USER").build(),
+                User.withUsername("sara").password(passwordEncoder.encode("sara")).roles("USER").build(),
+                User.withUsername("amanda").password(passwordEncoder.encode("amanda")).roles("USER").build()
+                );
+    }
+
+}
